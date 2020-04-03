17752c8d866ec330bac214cb8fabbc5debc6bcbb
Douglas Robert di Franco <douglas.franco@pampa.compasso>
2020-04-03 13:22:01 -0300

    Inclusão de conteúdo, ajuste ortografia e seção Equipe

--- js/custom.js
+++ js/custom.js
@@ -38,10 +38,11 @@
   // Team Slider
   $('.autoplay').slick({
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
     slidesToShow: 2,
-    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
     responsive: [{
         breakpoint: 1024,
         settings: {
