7fadb54e1cee326c7d88372a3ccef0802edda546
Douglas Robert di Franco <douglas.rfranco@gmail.com>
2020-04-07 13:49:39 -0300

    Ajuste carousel para mobile

--- js/custom.js
+++ js/custom.js
@@ -44,22 +44,6 @@
     autoplay: true,
     autoplaySpeed: 3000,
     responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
